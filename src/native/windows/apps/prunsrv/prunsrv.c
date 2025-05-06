5c0e7501831b8137a206f5d9e7f5decb11c7fa2e
Jean-Frederic Clere <jfclere@gmail.com>
2025-05-06 13:04:01 +0200

    test

--- src/native/windows/apps/prunsrv/prunsrv.c
+++ src/native/windows/apps/prunsrv/prunsrv.c
@@ -322,6 +322,7 @@ static BOOL redirectStdStreams(APX_STDWRAP *lpWrapper, LPAPXCMDLINE lpCmdline)
     if (GetConsoleWindow() == NULL) {
         HWND hc;
         AllocConsole();
+        apxLogWrite(APXLOG_MARK_INFO "Test");
         if ((hc = GetConsoleWindow()) != NULL)
             ShowWindow(hc, SW_HIDE);
     }
